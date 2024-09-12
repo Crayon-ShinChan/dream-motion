218845b1df6e51545e07e400a447700e11c1af46
Kuiliang Zhang <z1219202167@outlook.com>
2024-09-12 13:48:23 -0700

    fix: hydration error for Bleed

--- components/container.tsx
+++ components/container.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { ChartThemeProvider } from "@/components/chart-theme-provider";
+import { useHydration } from "@/hooks/hydration";
 import { Bleed } from "nextra-theme-docs";
 import { isMobile } from "react-device-detect";
 
 export default function Container({ children }: { children: React.ReactNode }) {
+  const hydrated = useHydration();
+
+  // TODO: fix this with server side headers after migrate to Nextra v4
   return (
     <ChartThemeProvider>
-      {isMobile ? (
+      {isMobile && hydrated ? (
         <Bleed full={false}>
           <div className="relative border-y border-zinc-200 dark:border-zinc-800 overflow-hidden">
             {children}
