8119b2f481407cedb4c1bbca0d12b2b92b0d3843
Kuiliang Zhang <z1219202167@outlook.com>
2024-09-13 17:11:58 -0700

    feat: allow override style of container

--- components/container.tsx
+++ components/container.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { ChartThemeProvider } from "@/components/chart-theme-provider";
+import { cn } from "@/lib/utils";
 
-export default function Container({ children }: { children: React.ReactNode }) {
+export default function Container({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
   return (
     <ChartThemeProvider>
-      <div className="relative mt-6 -mx-6 sm:mx-0 rounded-none sm:rounded-xl border-y sm:border border-zinc-200 dark:border-zinc-800 overflow-hidden">
+      <div
+        className={cn(
+          "relative mt-6 -mx-6 sm:mx-0 rounded-none sm:rounded-xl border-y sm:border border-zinc-200 dark:border-zinc-800 overflow-hidden",
+          className
+        )}
+      >
         {children}
       </div>
     </ChartThemeProvider>
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,6 @@ export default {
   logo: (
     <div className="flex items-center gap-2">
       <span className="font-bold text-2xl">Visual Gallery</span>
-      <span className="text-xs rounded-md bg-gray-200 dark:bg-zinc-800 px-2 py-1">
-        MengXi
-      </span>
     </div>
   ),
   project: {
